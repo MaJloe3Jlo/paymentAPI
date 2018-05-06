879074fd27bec4f9ea927b2b64686eb6cebbaf68
MaJloe3Jlo <rocsana555@mail.ru>
2018-05-06 20:48:26 +0300

    Changes after codereview_3

--- main.go
+++ main.go
@@ -22,10 +22,7 @@ func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/block/", block)
 	http.HandleFunc("/charge/", charge)
-	log.Println("Server started at http://localhost:7000")
-	log.Println("POST: methods /block/, /charge/; contentType: application/json")
-	log.Println("control json-requests look in path ./jsons")
-	log.Println("to test app you can use curl or postman")
+	log.Println("Server started at http://localhost:7000\nPOST: methods /block/, /charge/; contentType: application/json\ncontrol json-requests look in path ./jsons")
 	log.Fatal(http.ListenAndServe(":7000", nil))
 }
 
@@ -52,35 +49,41 @@ func block(w http.ResponseWriter, req *http.Request) {
 		return
 	} else if !strings.Contains(string(body), "card") {
 		log.Print("JSON not correct: field card doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field card doesn't exist")
 		return
 	} else if !strings.Contains(string(body), "pan") {
 		log.Print("JSON not correct: field pan doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field pan doesn't exist")
 		return
 	} else if !strings.Contains(string(body), "e_month") {
 		log.Print("JSON not correct: field e_month doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field e_monnth doesn't exist")
 		return
 	} else if !strings.Contains(string(body), "e_year") {
 		log.Print("JSON not correct: field e_year doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field e_year doesn't exist")
 		return
 	} else if !strings.Contains(string(body), "cvv") {
 		log.Print("JSON not correct: field cvv doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field cvv doesn't exist")
 		return
 	} else if !strings.Contains(string(body), "holder") {
 		log.Print("JSON not correct: field holder doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field holder doesn't exist")
 		return
 	} else if !strings.Contains(string(body), "order_id") {
 		log.Print("JSON not correct: field order_id doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field order_id doesn't exist")
+		return
 	} else if !strings.Contains(string(body), "amount") {
 		log.Print("JSON not correct: field amount doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field amount doesn't exist")
 		return
 	}
 
 	if err != nil {
-		file, errFile := ioutil.ReadFile("./jsons/block.json")
-		if errFile != nil {
-			log.Println(errFile)
-		}
-		defer log.Print("JSON isn't correct: " + err.Error() + ". JSON example: " + string(file))
+		defer log.Print("JSON isn't correct: " + err.Error() + ". JSON example: " + `{"merchant_contact_id": 1,"card": {"pan": "5469345678901234","e_month": 6,"e_year": 2020,"cvv": 332,"holder": "DMITRIY KLESTOV"},"order_id": "BuyMeTee123","amount": 99}`)
+		fmt.Fprint(w, "JSON isn't correct: "+err.Error()+". JSON example: "+`{"merchant_contact_id": 1,"card": {"pan": "5469345678901234","e_month": 6,"e_year": 2020,"cvv": 332,"holder": "DMITRIY KLESTOV"},"order_id": "BuyMeTee123","amount": 99}`)
 	}
 	defer req.Body.Close()
 
@@ -88,13 +91,18 @@ func block(w http.ResponseWriter, req *http.Request) {
 	if respBlock.DealID != -1 {
 		log.Printf("Block status: deal ID: %v, amount: %v, error(if nil operation ok): %v", respBlock.DealID, respBlock.Amount, respBlock.Error)
 		Block = append(Block, respBlock)
+		pretty, err := json.MarshalIndent(respBlock, "", "    ")
+		if err != nil {
+			log.Println(err.Error())
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, string(pretty))
 	} else {
 		log.Print("JSON request isn't valid")
+		fmt.Fprint(w, "JSON request isn't valid")
 		respBlock.Error = append(respBlock.Error, err.Error())
 	}
-
-	log.Println(reqBlock)
-
 }
 
 //charge - метод списания средств с виртуальной карты авторизованных методом block
@@ -109,17 +117,17 @@ func charge(w http.ResponseWriter, req *http.Request) {
 	err = json.Unmarshal(body, &reqCharge)
 	if !strings.Contains(string(body), "deal_id") {
 		log.Print("JSON not correct: field deal_id doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field deal_id doesn't exist")
 		return
 	} else if !strings.Contains(string(body), "amount") {
 		log.Print("JSON not correct: field amount doesn't exist")
+		fmt.Fprint(w, "JSON not correct: field amount doesn't exist")
 		return
 	}
 	if err != nil {
-		file, errFile := ioutil.ReadFile("./jsons/charge.json")
-		if errFile != nil {
-			log.Println(errFile)
-		}
-		defer log.Print("JSON isn't correct: " + err.Error() + ". JSON example: " + string(file))
+		defer log.Print("JSON isn't correct: " + err.Error() + ". JSON example: " + `{"deal_id": 5577006791947779410, "amount": 9}`)
+		fmt.Fprint(w, "JSON isn't correct: "+err.Error()+". JSON example: "+`{"deal_id": 5577006791947779410, "amount": 9}`)
+		return
 	}
 	defer req.Body.Close()
 
@@ -129,30 +137,59 @@ func charge(w http.ResponseWriter, req *http.Request) {
 			respCharge.Error = "Charge not working. Do not have this dealID"
 			Charge = append(Charge, &respCharge)
 			log.Printf("DealID: %v, charge status: %s, error description: %s", v.DealID, respCharge.Status, respCharge.Error)
-		} else if v.Amount < reqCharge.Amount {
-			respCharge.Status = "error"
-			respCharge.Error = "Charge not working. Amount of charge is bigger than amount of block"
-			Charge = append(Charge, &respCharge)
-			log.Printf("DealID: %v, charge status: %s, error description: %s", v.DealID, respCharge.Status, respCharge.Error)
-		} else {
-			go doReq(buf)
-			respCharge.Status = <-buf
-			if reqCharge.Amount < 0 {
-				v.Amount += reqCharge.Amount
-			} else {
-				v.Amount -= reqCharge.Amount
-			}
-			Charge = append(Charge, &respCharge)
-			log.Printf("DealID: %v, charge status: %s, amount balance: %v", v.DealID, respCharge.Status, v.Amount)
-			pretty, err := json.MarshalIndent(Charge, "", "    ")
+			pretty, err := json.MarshalIndent(respCharge, "", "    ")
 			if err != nil {
 				log.Println(err.Error())
 			}
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			fmt.Fprint(w, string(pretty))
+			return
+		} else if v.Amount < reqCharge.Amount {
+			respCharge.Status = "error"
+			respCharge.Error = "Charge not working. Amount of charge is bigger than amount of block"
+			Charge = append(Charge, &respCharge)
+			log.Printf("DealID: %v, charge status: %s, error description: %s", v.DealID, respCharge.Status, respCharge.Error)
+			pretty, err := json.MarshalIndent(respCharge, "", "    ")
+			if err != nil {
+				log.Println(err.Error())
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, string(pretty))
+			return
+		} else {
+			go doReq(buf)
+			respCharge.Status = <-buf
+			if reqCharge.Amount < 0 {
+				respCharge.Status = "error"
+				respCharge.Error = "Charge not working. Amount < 0"
+				log.Printf("DealID: %v, charge status: %s, error description: %s", v.DealID, respCharge.Status, respCharge.Error)
+				pretty, err := json.MarshalIndent(respCharge, "", "    ")
+				if err != nil {
+					log.Println(err.Error())
+				}
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprint(w, string(pretty))
+				return
+			} else {
+				v.Amount -= reqCharge.Amount
+			}
+			Charge = append(Charge, &respCharge)
+			log.Printf("DealID: %v, charge status: %s, amount balance: %v", v.DealID, respCharge.Status, v.Amount)
+			pretty, err := json.MarshalIndent(respCharge, "", "    ")
+			if err != nil {
+				log.Println(err.Error())
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, string(pretty))
+			return
 		}
 	}
+	log.Println("Cannot find this DealID. Use operation method block before method charge.")
+	fmt.Fprint(w, "Cannot find this DealID. Use operation method block before method charge.")
 }
 
 //doReq - метод фоновой отправки запроса к ya.ru
@@ -162,5 +199,6 @@ func doReq(buf chan string) {
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println("Request to ya.ru " + req.Status)
 	buf <- req.Status
 }
